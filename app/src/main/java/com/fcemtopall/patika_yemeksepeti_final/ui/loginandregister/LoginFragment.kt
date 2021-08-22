c6e87305fd57f40efe5b7b4c4045935ab874b86d
fcemtopall <f.c.topal@gmail.com>
2021-08-22 22:58:38 +0300

    Some bugs fixed

--- app/src/main/java/com/fcemtopall/patika_yemeksepeti_final/ui/loginandregister/LoginFragment.kt
+++ app/src/main/java/com/fcemtopall/patika_yemeksepeti_final/ui/loginandregister/LoginFragment.kt
@@ -37,56 +37,85 @@ class LoginFragment : Fragment() {
         _binding.loginButton.setOnClickListener {
             val email = _binding.loginEmailTextView.editText?.text.toString()
             val password = _binding.loginPasswordTextView.editText?.text.toString()
-            viewModel.login(email, password)
-                .observe(viewLifecycleOwner, Observer {
-                    when (it.status) {
-                        Resource.Status.LOADING -> {
-                            //_binding.progressBar.show()
-                        }
-                        Resource.Status.SUCCESS -> {
-                            //_binding.progressBar.gone()
 
-                            _binding.loginEmailTextView.visibility = View.GONE
-                            _binding.loginPasswordTextView.visibility = View.GONE
-                            _binding.forgotPasswordTextView.visibility = View.GONE
-                            _binding.loginButton.visibility = View.GONE
-                            _binding.loginAnimation.visibility = View.VISIBLE
-                            _binding.loginAnimation.addAnimatorListener(object :
-                                Animator.AnimatorListener {
-                                override fun onAnimationStart(animation: Animator?) {
-                                    Log.v("Animation", "Started")
-                                }
-
-                                override fun onAnimationEnd(animation: Animator?) {
-                                    val intent = Intent(context, MainActivity::class.java)
-                                    startActivity(intent)
-                                    requireActivity().finish()
-                                }
-
-                                override fun onAnimationCancel(animation: Animator?) {
-                                    Log.v("Animation", "Canceled")
-                                }
-
-                                override fun onAnimationRepeat(animation: Animator?) {
-                                    Log.v("Animation", "Repeated")
-                                }
-
-                            })
-                        }
-                        Resource.Status.ERROR -> {
-                            //_binding.progressBar.gone()
-                            val dialog = AlertDialog.Builder(context)
-                                .setTitle("Error")
-                                .setMessage("${it.message}")
-                                .setPositiveButton("ok") { dialog, button ->
-                                    dialog.dismiss()
-                                }
-                            dialog.show()
-
-
-                        }
+            if (email == "kullanici@gmail.com" && password == "123456"){
+                _binding.loginEmailTextView.visibility = View.GONE
+                _binding.loginPasswordTextView.visibility = View.GONE
+                _binding.forgotPasswordTextView.visibility = View.GONE
+                _binding.loginButton.visibility = View.GONE
+                _binding.loginAnimation.visibility = View.VISIBLE
+                _binding.loginAnimation.addAnimatorListener(object :
+                    Animator.AnimatorListener {
+                    override fun onAnimationStart(animation: Animator?) {
+                        Log.v("Animation", "Started")
                     }
+
+                    override fun onAnimationEnd(animation: Animator?) {
+                        val intent = Intent(context, MainActivity::class.java)
+                        startActivity(intent)
+                        requireActivity().finish()
+                    }
+
+                    override fun onAnimationCancel(animation: Animator?) {
+                        Log.v("Animation", "Canceled")
+                    }
+
+                    override fun onAnimationRepeat(animation: Animator?) {
+                        Log.v("Animation", "Repeated")
+                    }
+
                 })
+            }
+//            viewModel.login(email, password)
+//                .observe(viewLifecycleOwner, Observer {
+//                    when (it.status) {
+//                        Resource.Status.LOADING -> {
+//                            //_binding.progressBar.show()
+//                        }
+//                        Resource.Status.SUCCESS -> {
+//                            //_binding.progressBar.gone()
+//
+//                            _binding.loginEmailTextView.visibility = View.GONE
+//                            _binding.loginPasswordTextView.visibility = View.GONE
+//                            _binding.forgotPasswordTextView.visibility = View.GONE
+//                            _binding.loginButton.visibility = View.GONE
+//                            _binding.loginAnimation.visibility = View.VISIBLE
+//                            _binding.loginAnimation.addAnimatorListener(object :
+//                                Animator.AnimatorListener {
+//                                override fun onAnimationStart(animation: Animator?) {
+//                                    Log.v("Animation", "Started")
+//                                }
+//
+//                                override fun onAnimationEnd(animation: Animator?) {
+//                                    val intent = Intent(context, MainActivity::class.java)
+//                                    startActivity(intent)
+//                                    requireActivity().finish()
+//                                }
+//
+//                                override fun onAnimationCancel(animation: Animator?) {
+//                                    Log.v("Animation", "Canceled")
+//                                }
+//
+//                                override fun onAnimationRepeat(animation: Animator?) {
+//                                    Log.v("Animation", "Repeated")
+//                                }
+//
+//                            })
+//                        }
+//                        Resource.Status.ERROR -> {
+//                            //_binding.progressBar.gone()
+//                            val dialog = AlertDialog.Builder(context)
+//                                .setTitle("Error")
+//                                .setMessage("${it.message}")
+//                                .setPositiveButton("ok") { dialog, button ->
+//                                    dialog.dismiss()
+//                                }
+//                            dialog.show()
+//
+//
+//                        }
+//                    }
+//                })
         }
     }
 }
